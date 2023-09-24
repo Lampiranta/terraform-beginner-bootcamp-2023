5e9dadf4510da8edb6c00bb1199d1ccb54ac3440
Lampiranta <topi.lampiranta@gmail.com>
2023-09-24 17:05:49 +0000

    #21 create root module structure files

--- main.tf
+++ main.tf
@@ -1,29 +1,4 @@
-terraform {
-  cloud {
-    organization = "Lampiranta"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
 
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
@@ -40,11 +15,7 @@ resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    UserUUID = var.user_uuid
   }
 }
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
