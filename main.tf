d493cd50fd57277f2f2ad5c5cdcb83a43e50e76e
Lampiranta <topi.lampiranta@gmail.com>
2023-10-06 19:07:58 +0000

    #44 finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 terraform {
-required_providers {
-  terratowns = {
-    source = "local.providers/local/terratowns"
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
   }
-}
 }
 
 #module "terrahouse_aws" {
@@ -17,7 +18,7 @@ required_providers {
 #}
   
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -25,3 +26,15 @@ provider "terratowns" {
 #resource "aws_s3_bucket" "website_bucket" {
 #  # (resource arguments)
 #}
+
+resource "terratowns_home" "home" {
+  name = "The Lord of the Rings The Card Game"
+  description = <<DESCRIPTION
+The Lord of the Rings The Card Game is a living card game by Fantasy Flight Games.
+It is a cooperative card game in a fantasy world created by Professor J.R.R Tolkien.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
